5a4f781612274871917d2addcc77c96cefb4eacb
Renato França <renato.franca@frannca.com.br>
2018-08-07 17:27:08 -0400

    feat: add sidebar function

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,13 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { TemplateComponent } from './template.component';
@@ -7,6 +15,12 @@ import { TemplateComponent } from './template.component';
 @NgModule({
   imports: [
     CommonModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
     DashboardRoutingModule
   ],
   declarations: [
--- src/app/dashboard/template.component.ts
+++ src/app/dashboard/template.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout';
+import {
+  Component,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.scss']
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent implements OnDestroy {
 
-  constructor() { }
+  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
 
-  ngOnInit() {
+  fillerContent = Array.from({length: 50}, () =>
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(Breakpoints.Handset).subscribe(result => {
+      if (result.matches) {
+        console.log('handset');
+      }
+    });
+
+    breakpointObserver.observe(Breakpoints.Tablet).subscribe(result => {
+      if (result.matches) {
+        console.log('tablet');
+      }
+    });
+
+    breakpointObserver.observe(Breakpoints.Web).subscribe(result => {
+      if (result.matches) {
+        console.log('web');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
