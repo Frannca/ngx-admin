da75ae20155c6b94376fa066edb0a1298cbfebce
Renato França <renato.franca@frannca.com.br>
2018-08-13 19:26:51 -0400

    refactor: create navigation page and empty dashboard

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,5 +1,11 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatIconModule
+} from '@angular/material';
 
 import { NavigationRoutingModule } from './navigation-routing.module';
 import { NavigationComponent } from './navigation.component';
@@ -8,7 +14,11 @@ import { TemplateComponent } from './template.component';
 @NgModule({
   imports: [
     CommonModule,
-    NavigationRoutingModule
+    LayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    NavigationRoutingModule,
+    FlexLayoutModule
   ],
   declarations: [
     NavigationComponent,
